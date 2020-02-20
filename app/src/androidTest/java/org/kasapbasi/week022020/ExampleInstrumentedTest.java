a9ac17caab20c45e258671ad930e552bc7a00784
mckasap <mckasap@gmail.com>
2020-02-20 14:26:58 +0300

    Second Commit,  Locale ile calışmak Çoklu dil örneği

--- app/src/androidTest/java/org/kasapbasi/week022020/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/kasapbasi/week022020/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package org.kasapbasi.week022020;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
